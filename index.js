e41116a8b518079353b479f0f381b235016f0391
faizanshahid232 <75093651+faizanshahid232@users.noreply.github.com>
2023-07-20 02:23:04 +0400

    fifh commit

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 app.listen(process.env.PORT || 3000);
 const cors = require('cors');
+// This is your test secret API key.
+const stripe = require("stripe")('sk_test_51NOz1fIi4beyPjru1XAPSAdxY1x8zH8fJMOghajQGbgq2SVgE3R2tLTj8fhoZ8kCJHq7wX0PKgstks4S6NUBwRYA006SNgD679');
+
+app.use(express.static("public"));
+app.use(express.json());
 
 var whitelist = [
     '*',
@@ -15,6 +20,32 @@ var corsOptions = {
 };
 app.use(cors(corsOptions));
 
+const calculateOrderAmount = (items) => {
+    // Replace this constant with a calculation of the order's amount
+    // Calculate the order total on the server to prevent
+    // people from directly manipulating the amount on the client
+    return items;
+  };
+  
+  app.post("/create-payment-intent", async (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    const { items } = req.body;
+      console.log("here1", items);
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: calculateOrderAmount(items),
+      currency: "usd",
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+    console.log("Payment:", paymentIntent.client_secret);
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+      
+    });
+  });
+  
     const ingredients = [
     {
         "id": "1",
