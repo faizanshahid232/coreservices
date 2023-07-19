6e8201de8459f423846277eaaa6ea934c413221c
faizanshahid232 <75093651+faizanshahid232@users.noreply.github.com>
2023-07-20 02:30:58 +0400

    ten commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 app.listen(process.env.PORT || 3000);
 const cors = require('cors');
 // This is your test secret API key.
-//const stripe = require("stripe")('sk_test_51NOz1fIi4beyPjru1XAPSAdxY1x8zH8fJMOghajQGbgq2SVgE3R2tLTj8fhoZ8kCJHq7wX0PKgstks4S6NUBwRYA006SNgD679');
+
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -19,6 +19,8 @@ var corsOptions = {
     credentials: true
 };
 app.use(cors(corsOptions));
+
+const stripe = require("stripe")('sk_test_51NOz1fIi4beyPjru1XAPSAdxY1x8zH8fJMOghajQGbgq2SVgE3R2tLTj8fhoZ8kCJHq7wX0PKgstks4S6NUBwRYA006SNgD679');
 
 /*const calculateOrderAmount = (items) => {
     // Replace this constant with a calculation of the order's amount
