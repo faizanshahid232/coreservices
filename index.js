bc39182311945478163b6e08145bb8c79390e1b8
faizanshahid232 <75093651+faizanshahid232@users.noreply.github.com>
2023-07-20 02:08:14 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+var whitelist = [
+    '*',
+];
+var corsOptions = {
+    origin: function(origin, callback){
+        var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+        callback(null, originIsWhitelisted);
+    },
+    credentials: true
+};
+app.use(cors(corsOptions));
+
+    const ingredients = [
+    {
+        "id": "1",
+        "item": "Bacon"
+    },
+    {
+        "id": "2",
+        "item": "Eggs"
+    },
+    {
+        "id": "3",
+        "item": "Milk"
+    },
+    {
+        "id": "4",
+        "item": "Butter"
+    }
+];
+
+app.get('/ingredients', cors(), (req, res, next) =>{
+    res.send(ingredients);
+});
+app.listen(6069);
