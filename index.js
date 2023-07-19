e863f7d87bedf5f5c6afa17a71a665c02ae81930
faizanshahid232 <75093651+faizanshahid232@users.noreply.github.com>
2023-07-20 02:32:15 +0400

    ele commit

--- index.js
+++ index.js
@@ -20,8 +20,6 @@ var corsOptions = {
 };
 app.use(cors(corsOptions));
 
-const stripe = require("stripe")('sk_test_51NOz1fIi4beyPjru1XAPSAdxY1x8zH8fJMOghajQGbgq2SVgE3R2tLTj8fhoZ8kCJHq7wX0PKgstks4S6NUBwRYA006SNgD679');
-
 /*const calculateOrderAmount = (items) => {
     // Replace this constant with a calculation of the order's amount
     // Calculate the order total on the server to prevent
@@ -51,6 +49,9 @@ const stripe = require("stripe")('sk_test_51NOz1fIi4beyPjru1XAPSAdxY1x8zH8fJMOgh
 app.get('/ingredients', cors(), (req, res, next) =>{
     res.send(ingredients);
 });
+
+const stripe = require("stripe")('sk_test_51NOz1fIi4beyPjru1XAPSAdxY1x8zH8fJMOghajQGbgq2SVgE3R2tLTj8fhoZ8kCJHq7wX0PKgstks4S6NUBwRYA006SNgD679');
+
 
 /*app.post("/create-payment-intent", async (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
