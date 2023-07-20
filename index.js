d12ec5f4f1b116c7c6ecdf01cb70b13f16208543
faizanshahid232 <75093651+faizanshahid232@users.noreply.github.com>
2023-07-20 16:59:40 +0400

    twl2 commit

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.get('/cors', (req, res) => {
 const stripe = require("stripe")('sk_test_51NOz1fIi4beyPjru1XAPSAdxY1x8zH8fJMOghajQGbgq2SVgE3R2tLTj8fhoZ8kCJHq7wX0PKgstks4S6NUBwRYA006SNgD679');
 
 app.post("/create-payment-intent", cors(), async (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Origin', 'https://payment-ten-sooty.vercel.app/');
     const { items } = req.body;
       console.log("here1", items);
     // Create a PaymentIntent with the order amount and currency
